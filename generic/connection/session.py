8c738c6b3ca9f2e1cac446c3520c80a410866670
Gabriela Vilaró <50081357+GabrielaVilaro@users.noreply.github.com>
2020-12-05 11:48:32 -0300

    Update session.py

--- generic/connection/session.py
+++ generic/connection/session.py
@@ -3,7 +3,7 @@ import boto3
 
 class Session:
     def __init__(self):
-        self.session = boto3.Session(profile_name="uala-mex-debit-test", region_name="us-east-1")
+        self.session = boto3.Session(profile_name="mex-debit-test", region_name="us-east-1")
         self.resource = self.session.resource('dynamodb')
         self.client = self.session.client('dynamodb')
         self.table = self.resource.Table('testUsers')
